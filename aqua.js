28cf70cff857971f81cc85731ca3cdd1f1521629
Chuck Konkol <ckonkol@gmail.com>
2022-10-12 14:59:35 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -222,7 +222,7 @@
             var removewebsiteYes = "https://ignitevisitorsystem.github.io/?id=" + doc.data().key + "&Remove=Yes";
            var removewebsiteNo = "https://ignitevisitorsystem.github.io/?id=" + doc.data().key + "&Remove=No";
 	var video = "https://youtu.be/6hxZn-wAfwY";
-	document.getElementById("emaillink").innerHTML = "<a href='mailto:" + doc.data().email + "?subject=Upcoming Kids Church at Ignite Church" + '' + "&body=" + doc.data().firstname + " " + doc.data().lastname  + "%0D%0A%0D%0AA unique QR code can be used to Check-in at the iPad stand in our lobby.%0D%0A%0D%0APlease use the below link to get your QR code.%0D%0A" + document.getElementById("bitly").value + "%0D%0A%0D%0AWatch below video to learn how to use our check-in/check-out system:%0D%0A" + video + "'>Click here to create email to guest...</a>";
+	document.getElementById("emaillink").innerHTML = "<a href='mailto:" + doc.data().email + "?subject=Upcoming Kids Church at Ignite Church" + '' + "&body=" + doc.data().firstname + " " + doc.data().lastname  + "%0D%0A%0D%0AA unique QR code can be used to Check-in at the iPad stand in our lobby.%0D%0A%0D%0APlease use the below link to get your QR code.%0D%0A" + document.getElementById("bitly").value + "'>Click here to create email to guest...</a>";
         console.log("Remove:" + doc.data().remove);
           if (doc.data().remove === 'Yes'){
             document.getElementById('qrcode').style.display = 'none';
