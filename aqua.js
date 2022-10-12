8d5c181c1484ace78edbd8b0b365a7a54d55a996
Chuck Konkol <ckonkol@gmail.com>
2022-10-12 05:41:46 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -237,7 +237,6 @@
               document.getElementById("removeYes").innerHTML = "<a href='" + removewebsiteYes + "'>Click here to update status to: InActive</a><br>";
           }
          if (get_iPad === 'Yes'){
-		    document.getElementById("date").readOnly = false;
 		   document.getElementById('qrcode').style.display = 'none';
                document.getElementById('logins').style.display = 'none';
              document.getElementById('removeYes').style.display = 'none';
