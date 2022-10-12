a9b83c20ac2a414db2339e8258409abe664d8413
Chuck Konkol <ckonkol@gmail.com>
2022-10-12 10:27:58 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -641,9 +641,7 @@ var utcTime = date.toUTCString();
            var db = firebase.firestore();
 	 let todaysdate = new Date();
 	 var count = 0;
-	
-         console.log(end);
-         console.log(start);		  
+
          var lines = "";
 	var today = new Date();
           console.log(today);
@@ -663,7 +661,9 @@ var end = new Date();
 const timestampInMs2 = end.setUTCHours(23,59,59,999);	
 const timestampInSeconds1 = Math.floor(timestampInMs / 1000);
 const timestampInSeconds2 = Math.floor(timestampInMs2 / 1000);
-  
+  	
+         console.log(timestampInSeconds1);
+         console.log(timestampInSeconds2);	
          var header = "<head><style>table, td, th {  border: 1px solid #cbbbbb;  text-align: left;}table {  border-collapse: collapse;  width: 100%;}th, td {  padding: 15px;} tr:nth-child(even) {  background-color: #dddddd;}</style></head>";
 	 var title = "<center><h2>Active Visitor(s) for: " + todays + "</h2><center><a href='https://ignitevisitorsystem.github.io/'>Go Home</a></center><br>";         
 	 db.collection("checkin").where("timestamp", ">",timestampInSeconds1).where("remove", "==","No").orderBy("timestamp","desc")
