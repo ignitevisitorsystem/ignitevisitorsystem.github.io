4e7702153c9cd616c34f9e48ef73bcc20b15561d
Chuck Konkol <ckonkol@gmail.com>
2022-10-19 11:26:48 -0500

    Update aqua.js

--- aqua.js
+++ aqua.js
@@ -338,9 +338,9 @@ var utcTime = date.toUTCString();
 	  //document.cookie = "YOUR_COOKIE=yes; expires=" + utcTime + ";";
 	   document.write("<center>");
 	  document.write('<img id="logo" src="ignite.jpg" width="320px">');
-	    document.write("<p style='font-size:45px;line-height: 0;'><b>" + varFName + " " + varLName + "</b></p>");
+	     document.write("<p style='font-size:45px;line-height: 0.9;margin: 15;'>Guest: <b>" + varFName + " " + varLName + "</b></p>");
 	  document.write('<canvas id="qrcodes"></canvas>');
-	  document.write("<p style='font-size:25px;color: black;'>Parent/Guardian: " + varcp + "</p>");
+	  document.write("<p style='font-size:25px;color: black;margin: 15;'>Parent/Guardian: " + varcp + "</p>");
 	  // document.write("<p style='font-size:16px;color: black;'><br><br><br>printed: " + NowTime + "</p></center>");
    var qrcode = new QRious({
   element: document.getElementById("qrcodes"),
